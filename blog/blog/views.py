e11393bb152b5e1735b41e53360acdc9bd7c1245
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-20 19:28:19 -0300

    vistas

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
-def inicio(request):
-	return render(request, "inicio.html")
+def inicio(request):	
+	post = [
+		{"nombre": "Post 1", "cuerpo": "Este es el cuerpo 1"},
+		{"nombre": "Post 2", "cuerpo": "Este es el cuerpo 2"},
+		
+	]
+
+	usuario = {
+		"nombre": "Carlos",
+		"apellido": "Kesqui"
+	}
+
+	context = {
+		"usuario": usuario,
+		"post": post
+	}
+
+	return render(request, "inicio.html", context)
