ee1d72d12dbdac66e2d7b209e3cfef3929cee34a
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-20 20:01:05 -0300

    cambios

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "blog",
+        'USER': "postgres",
+        'PASSWORD': "a",
+        'HOST': "localhost",
+        'PORT': "5432"
     }
 }
 
