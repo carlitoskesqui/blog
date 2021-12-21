83333457e740ef01abedb99005f4695dd0d9363a
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-21 20:20:34 -0300

    cambios

--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts 		import render
-from django.views.generic 	import ListView
+from django.views.generic 	import ListView, CreateView
 from .models 				import Post
 
 def detalle(request):	
@@ -10,3 +10,11 @@ class ListarAdmin(ListView):
 	template_name="post/admin/listar.html"
 	model = Post
 	context_object_name="post"
+
+	"""
+	Filtrar lista:
+	def get_queryset(self):
+		return Post.objects.filter(id=2)
+	"""
+
+class NuevoAdmin(CreateView)
