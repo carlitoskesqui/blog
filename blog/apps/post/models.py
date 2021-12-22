427fd4b75299b8d91df7ade1309a4fb5501c555a
Gonxa <Gonza@example.com>
2021-12-22 20:18:38 -0300

    se agrego class_categorias

--- blog/apps/post/models.py
+++ blog/apps/post/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	class Meta:
+		db_table="categorias"
+
+	def __str__(self):
+		return self.nombre
+	
 
 class Post(models.Model):
 	titulo = models.CharField(max_length=250)
@@ -7,6 +16,12 @@ class Post(models.Model):
 	fecha_creacion = models.TimeField(auto_now_add=True)
 	autor= models.CharField(max_length=100)
 	thumbnail = models.ImageField()
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
 
 	class Meta:
 		db_table="post"
+
+	def __str__(self):
+		return self.nombre
+
+
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "postgres",
         'USER': "postgres",
-        'PASSWORD': "1",
+        'PASSWORD': "2429",
         'HOST': "localhost",
         'PORT': "5432"
     }
