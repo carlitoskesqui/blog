e0dd58092254da33b7701dae35823f6cb79c7238
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-21 21:11:33 -0300

    nuevo post

--- blog/apps/post/urls.py
+++ blog/apps/post/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls    import path
+from .              import views
 
 app_name = "post"
 
 urlpatterns = [
     path('detalle/', views.detalle, name='detalle'),
+
+    # Usuario Registrado
     path('admin/listar', views.ListarAdmin.as_view(), name='admin_listar'),
+    path('admin/nuevo', views.NuevoPost.as_view(), name='admin_nuevo'),
 ]
--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts 		import render
 from django.views.generic 	import ListView, CreateView
 from .models 				import Post
+from .forms 				import PostForm
+from django.urls 			import reverse_lazy
 
 def detalle(request):	
 	context = {}
@@ -17,4 +19,10 @@ class ListarAdmin(ListView):
 		return Post.objects.filter(id=2)
 	"""
 
-class NuevoAdmin(CreateView)
+class NuevoPost(CreateView):
+	template_name = "post/admin/nuevo.html"
+	model = Post
+	form_class = PostForm
+
+	def get_success_url(self, **kwargs):
+		return reverse_lazy("post:admin_listar")
