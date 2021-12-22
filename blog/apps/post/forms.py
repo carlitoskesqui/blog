8b45f58b88110135d10a143e221125b4c2bdf5d0
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-21 23:09:25 -0300

    cambios

--- blog/apps/post/forms.py
+++ blog/apps/post/forms.py
@@ -3,6 +3,9 @@ from .models 	import Post
 
 
 class PostForm(forms.ModelForm):
+	titulo = forms.CharField(label="Título", widget=forms.TextInput(attrs={"class":"form-control"}))
+	cuerpo = forms.CharField(label="Contenido", widget=forms.Textarea(attrs={'class':'form-control', 'rows': 5, 'cols': 100}))
+
 	class Meta:
 		model = Post
 		fields = ["titulo", "cuerpo"]
