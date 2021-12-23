7e7b1505b122e2d6ef8c8d0b61de9cb02de417de
carlitoskesqui <carlitoskesqui@gmail.com>
2021-12-23 01:10:50 -0300

    01:10

--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -5,11 +5,6 @@ from .models 					import Post
 from .forms 					import PostForm
 from django.urls 				import reverse_lazy
 
-def detalle(request):	
-	context = {}
-	return render(request, "post/detalle.html", context)
-
-
 
 class ListarAdmin(ListView):
 	template_name="post/admin/listar.html"
@@ -63,3 +58,20 @@ class VerPost(DetailView):
 
 	def get_success_url(self, **kwargs):
 		return reverse_lazy("post:admin_listar")
+
+
+class Detalle(DetailView):
+	template_name = "post/detalle.html"
+	model = Post
+
+	def get_context_data(self, **kwargs):
+		context = super(Detalle, self).get_context_data(**kwargs)
+		context["buscado"] = self.request.GET.get("nombre_post", "")
+		return context
+	
+	def get_queryset(self):
+		busqueda_nombre = self.request.GET.get("nombre_post", "")
+		query = Post.objects.all().order_by("titulo")
+		if len(busqueda_nombre) > 0:
+			query = query.filter(titulo__icontains=busqueda_nombre)
+		return query	
